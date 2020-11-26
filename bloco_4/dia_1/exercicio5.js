5a73a4612fece68573de5c6c9e5c760b41c6a253
MichaelMascarenhas <michaelmascarenhastrybe@gmail.com>
2020-11-26 18:13:15 -0300

    exercicios dia 1 JS

---
+++ bloco_4/dia_1/exercicio5.js
@@ -0,0 +1,12 @@
+let x = 30;
+let y = 75;
+let z = 70;
+let total = x + y + z;
+
+
+if (total == 180){
+   console.log("Estes angulos representam um triangulo!")
+} 
+else{
+    console.log("isto certamente não é um triangulo")
+}
