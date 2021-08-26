b6435fe8270290142c6857dfa981a3e857fbf9ba
Michael Mascarenhas <michaelmascarenhastrybe@gmail.com>
2021-08-26 08:19:53 -0300

    atualizando para questionFloat

--- back-end/Bloco_26/exercicios/26.1/imc.js
+++ back-end/Bloco_26/exercicios/26.1/imc.js
@@ -1,7 +1,7 @@
 const readline = require('readline-sync');
 
 const pesoEmKg = readline.question('Qual seu peso?');
-const alturaAoQuadrado = readline.question('Qual a sua altura?');
+const alturaAoQuadrado = readline.questionFloat('Qual a sua altura?');
 
 
 console.log(`Seu IMC atual é: ${ pesoEmKg/(alturaAoQuadrado * 2) }!`);
