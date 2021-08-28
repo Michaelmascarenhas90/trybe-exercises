a115b8718cb44395006b5bcb84f68313207f2fb6
Michael Mascarenhas <michaelmascarenhastrybe@gmail.com>
2021-08-28 15:47:54 -0300

    implementando o readline-sync

--- back-end/Bloco_26/funcao-segundo-grau/index.js
+++ back-end/Bloco_26/funcao-segundo-grau/index.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+const readLine = require("readline-sync");
+
+function executaCalculo() {
+    const a = readLine.questionInt("digite o valor de a: ");
+    const b = readLine.questionInt("digote o valor de b: ");
+    const c = readLine.questionInt("digite o valor de c: ");
+}
