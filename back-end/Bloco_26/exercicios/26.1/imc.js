8ff3140d2bd1ca0d6e83f440d024423fb84167f4
Michael Mascarenhas <michaelmascarenhastrybe@gmail.com>
2021-08-28 12:58:07 -0300

    rep pixel art

--- back-end/Bloco_26/exercicios/26.1/imc.js
+++ back-end/Bloco_26/exercicios/26.1/imc.js
@@ -1,7 +1,7 @@
 const readline = require('readline-sync');
 
 const pesoEmKg = readline.question('Qual seu peso?');
-const alturaAoQuadrado = readline.questionFloat('Qual a sua altura?');
+const alturaAoQuadrado = readline.questionFloat(`Qual a sua altura em centimetros?`);
 
 
-console.log(`Seu IMC atual é: ${ pesoEmKg/(alturaAoQuadrado * 2) }!`);
+console.log(`Seu IMC atual é: ${ pesoEmKg/((alturaAoQuadrado / 100)*2) }!`);
