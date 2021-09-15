0560185e87b99411ea3b9b4392d5780f607c2646
Michael Mascarenhas <michaelmascarenhastrybe@gmail.com>
2021-09-15 07:09:29 -0300

    tratando erros com rescue

--- back-end/Bloco_26/exercicios/26.5/erros/index.js
+++ back-end/Bloco_26/exercicios/26.5/erros/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-// import rescue from 'express-recue';
+import rescue from 'express-recue';
 import { promises as fs } from 'fs';
 
 import fetch from 'node-fetch';
@@ -10,7 +10,26 @@ app.use((error, req, res, next) => {
     return res.send(500).json({ message: 'erro desconhecido' })
 })
 
-app.get('/fazerRequest', async (req, res) => {
+app.get('/fazerRequest', rescue (async (req, res) => {
+    const { url } = req.query;
+    
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    res.status(202).json({ data });
+}));
+
+// usando rescue para tratar erros
+app.get('/lerArquivo', recue (async (req, res, next) => {
+    const { fileName } = req.query;
+
+    const content = await fs.readFile(fileName);
+    res.status(200).json({ content: content.toString() });
+}));
+
+//mesmas funções só que usando try catch
+
+/*app.get('/fazerRequest', async (req, res) => {
     try {
         const { url } = req.query;
     
@@ -19,16 +38,17 @@ app.get('/fazerRequest', async (req, res) => {
     
         res.status(202).json({ data });
     } catch (error) {
+        if(error.message = 'Api fora do ar')
+            return res.status(400).json({ message: 'Api fora do ar' });
+        else
+            res.status(500).send();
         // status 500 se refere a um erro não tratado, 
-        // possibilita manter o funcionamento da api mesmo após apresentar erro
-        res.status(500).send();
-        
+        // possibilita manter o funcionamento da api mesmo após apresentar erro  
     }
-    
+});*/
 
-});
 
-app.get('/lerArquivo', async (req, res, next) => {
+/*app.get('/lerArquivo', async (req, res, next) => {
     const { fileName } = req.query;
     try {
         const content = await fs.readFile(fileName);
@@ -38,6 +58,6 @@ app.get('/lerArquivo', async (req, res, next) => {
         // ser direcionado par o middleware especifico
         next(error);
     }
-})
+})*/
 
 app.listen(3002, () => {console.log('http://localhost:3002 -> onde a api esta rodando')});
