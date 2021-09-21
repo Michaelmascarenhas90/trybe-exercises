06b01a56b5d0ba71b7d6eabed57df8d58c7ea6e3
Michael Mascarenhas <michaelmascarenhastrybe@gmail.com>
2021-09-21 18:24:52 -0300

    inserindo arquivo svg em uma imagem

--- front-end/projeto-notes/notes/src/components/CardNotas/CardNotas.jsx
+++ front-end/projeto-notes/notes/src/components/CardNotas/CardNotas.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import "./style.css";
+import deleteSVG from "../../assets/img/delete.svg";
 
 class CardNotas extends Component {
     render() { 
@@ -8,6 +9,7 @@ class CardNotas extends Component {
             <section className="card-nota">
                 <header className="card-nota_cabecalho">
                     <h3 className="card-nota_titulo">{this.props.titulo}</h3>
+                    <img src={ deleteSVG } alt="svg-botao-delete" />
                 </header>
                 <p className="card-nota_texto">{this.props.text}</p>
             </section>
