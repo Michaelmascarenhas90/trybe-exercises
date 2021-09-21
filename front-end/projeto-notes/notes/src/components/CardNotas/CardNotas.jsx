16eb16765567456b39b4aa16df044a04f7964dfc
Michael Mascarenhas <michaelmascarenhastrybe@gmail.com>
2021-09-21 18:26:34 -0300

    inserindo svg como no card como um component

--- front-end/projeto-notes/notes/src/components/CardNotas/CardNotas.jsx
+++ front-end/projeto-notes/notes/src/components/CardNotas/CardNotas.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 import "./style.css";
-import deleteSVG from "../../assets/img/delete.svg";
+import {ReactComponent as DeleteSVG}from "../../assets/img/delete.svg";
 
 class CardNotas extends Component {
     render() { 
@@ -9,7 +9,7 @@ class CardNotas extends Component {
             <section className="card-nota">
                 <header className="card-nota_cabecalho">
                     <h3 className="card-nota_titulo">{this.props.titulo}</h3>
-                    <img src={ deleteSVG } alt="svg-botao-delete" />
+                    <DeleteSVG />
                 </header>
                 <p className="card-nota_texto">{this.props.text}</p>
             </section>
